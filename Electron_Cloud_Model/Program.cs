77c255eff69cd403c2aa23eea1bd33c15e80032b
JacksonRankin117 <ran23008@byui.edu>
2025-12-05 23:49:21 -0700

    More TV static. Moving on to Radial.cs

--- Electron_Cloud_Model/Program.cs
+++ Electron_Cloud_Model/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         Plot plot = new Plot();
 
-        double[,] data = plot.RandData(400, 400);
+        double[,] data = plot.RandData(1000, 1000);
         plot.PPMWriter(data, "/Users/jackson.rankin/Desktop/Student/6/CSE310/Electron_Cloud_Model/Electron_Cloud_Model/output.ppm");
     }
 }
